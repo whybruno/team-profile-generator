f3cdb359db13e64c1f2342b4b1c3ccbc49015812
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2024-02-12 18:48:28 +0000

    Add the absolute path to the output directory

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// Constructs the absolute path to the "output" directory
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+// Creates the full path to the "team.html" file within the "output" directory
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+// Imports a render function named "render" from a module named "page-template.js"
+const render = require("./src/page-template.js");
